3559b3b13c66688fb2186b415a510496f9bbfc18
Suresh Prajapati <51094657+sureshprajapati076@users.noreply.github.com>
2020-08-11 12:26:53 -0500

    m

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @PostMapping(value = "/users", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = "application/json")
+    public ResponseEntity saveUsers(@RequestParam(value = "files") MultipartFile[] files) throws Exception {
+        for (MultipartFile file : files) {
+            service.saveUsers(file);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(value = "/users", produces = "application/json")
+    public CompletableFuture<ResponseEntity> findAllUsers() {
+        return service.findAllUsers().thenApply(ResponseEntity::ok);
+    }
+
+
+    @GetMapping(value = "/getUsersByThread", produces = "application/json")
+    public ResponseEntity getUsers() {
+        CompletableFuture<List<User>> users1 = service.findAllUsers();
+        CompletableFuture<List<User>> users2 = service.findAllUsers();
+        CompletableFuture<List<User>> users3 = service.findAllUsers();
+        System.out.println(users1);
+        System.out.println(users1);
+        System.out.println(users1);
+
+
+        CompletableFuture.allOf(users1, users2, users3).join();
+        System.out.println(users1);
+        System.out.println(users1);
+        System.out.println(users1);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
